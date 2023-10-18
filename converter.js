ab793971824df05bbfe740e9f931d7b823d9823e
nishika052003 <112238066+nishika052003@users.noreply.github.com>
2023-10-18 14:33:20 +0530

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,11 @@
+let celsius = document.getElementById("celsius");
+let farenheit = document.getElementById("farenheit");
+
+function celToFar() {
+  let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+  farenheit.value = parseFloat(output.toFixed(2));
+}
+function farToCel() {
+  let output = ((parseFloat(farenheit.value) - 32) * 5) / 9;
+  celsius.value = parseFloat(output.toFixed(2));
+}
